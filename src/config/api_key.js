8e9cec7d2fec809f0793d66ae4b43244efeb9b90
Thilak-mani <thilakmani777@gmail.com>
2021-08-25 09:50:50 +0530

    deploying to github

--- src/config/api_key.js
+++ src/config/api_key.js
@@ -1,4 +1,4 @@
-const api_key = '5dd30dc0f2f0184480e1db469e258364';
+// const api_key = '5dd30dc0f2f0184480e1db469e258364';
 
 const main_key = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5dd30dc0f2f0184480e1db469e258364&page=1';
 const search_key = 'https://api.themoviedb.org/3/search/movie?&api_key=5dd30dc0f2f0184480e1db469e258364&query=';
