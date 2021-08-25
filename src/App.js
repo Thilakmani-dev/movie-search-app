464b90386e1dc1c6a6872d59b5a44fa5bb43019d
Thilak-mani <thilakmani777@gmail.com>
2021-08-25 09:59:47 +0530

    modified router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
 import Homepage from './components/HomePage';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import {HashRouter,Route,Switch} from 'react-router-dom';
 import MoviePage from './components/MoviePage';
 
 function App() {
   return (
-    <Router>
+    <div>
+      <HashRouter basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={Homepage}/>
         <Route exact path="/movie/:title" component={MoviePage}/>
       </Switch>
-    </Router>
+      </HashRouter>
+    </div>
   );
 }
 
